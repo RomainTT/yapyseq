49ab6a9f00b9acac2a20fb9671efd71779b83b8c
Romain TAPREST <romain@taprest.fr>
2018-08-12 18:32:36 +0200

    create basic files for python directory

---
+++ yapyseq/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='yapyseq',
+      version='0.1',
+      description='Yet Another Python Sequencer',
+      author='Romain TAPREST',
+      author_email='romain@taprest.fr',
+      url='',
+      packages=['yapyseq']
+     )
