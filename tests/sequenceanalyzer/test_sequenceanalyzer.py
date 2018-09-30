9c4439b9cb20299c1f35433050fd66827b84d2d0
Romain TAPREST <romain@taprest.fr>
2018-09-30 18:28:32 +0200

    [tests] Add many tests on SequenceAnalyzer init

--- tests/sequenceanalyzer/test_sequenceanalyzer.py
+++ tests/sequenceanalyzer/test_sequenceanalyzer.py
@@ -7,12 +7,29 @@ import os
 from yapyseq.sequenceanalyzer import *
 
 
+VALID_SEQ_PATH = "tests/sequenceanalyzer/sequences/valid"
+INVALID_SEQ_PATH = "tests/sequenceanalyzer/sequences/invalid"
+
+
 class TestSequenceAnalyzerInitAndCheck(object):
 
-    @pytest.mark.parametrize("seq_name", [
-        "valid/minimal.yaml",
-        "valid/minimal_with_optional.yaml"
-    ])
-    def test_valid_sequence(self, schema_path, seq_dir, seq_name):
-        seq_path = os.path.join(seq_dir, seq_name)
-        sa = SequenceAnalyzer(seq_path, schema_path)
+    @pytest.mark.parametrize("seq_name",
+                             os.listdir(VALID_SEQ_PATH))
+    def test_valid_sequence(self, schema_path, seq_name):
+        """Test the initialization of a valid sequence.
+
+        No exception must be raised.
+        """
+        seq_path = os.path.join(VALID_SEQ_PATH, seq_name)
+        SequenceAnalyzer(seq_path, schema_path)
+
+    @pytest.mark.parametrize("seq_name",
+                             os.listdir(INVALID_SEQ_PATH))
+    def test_invalid_sequence(self, schema_path, seq_name):
+        """Test the initialization of a valid sequence.
+
+        No exception must be raised.
+        """
+        seq_path = os.path.join(INVALID_SEQ_PATH, seq_name)
+        with pytest.raises(SequenceFileError):
+            SequenceAnalyzer(seq_path, schema_path)
