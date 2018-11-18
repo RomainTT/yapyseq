62449aff538fd769b4407e97ff02c078177c2013
Romain TAPREST <romain@taprest.fr>
2018-11-18 23:05:08 +0100

    Add click command 'run'

--- yapyseq/cli.py
+++ yapyseq/cli.py
@@ -10,7 +10,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import click
 import pkg_resources
 
-from yapyseq import SequenceReader, SequenceFileError
+from yapyseq import SequenceReader, SequenceFileError, SequenceRunner
 
 
 @click.group()
@@ -20,9 +20,12 @@ def yapyseq_main_cli():
 
 
 @yapyseq_main_cli.command()
-@click.argument('sequence_file')
+@click.argument('sequence_file', type=click.Path(exists=True))
 def check(sequence_file):
-    """Check content validity of a sequence file."""
+    """Check content validity of a sequence file.
+
+    SEQUENCE_FILE is the path to the sequence file to check.
+    """
     schema = pkg_resources.resource_filename('yapyseq', 'seq_schema.yaml')
     try:
         SequenceReader.check_sequence_file(sequence_file, schema)
@@ -41,3 +44,17 @@ def check(sequence_file):
         msg = click.style('Sequence file is valid !',
                           bold=True, fg='green')
         click.echo(msg)
+
+
+@yapyseq_main_cli.command()
+@click.argument('sequence_file', type=click.Path(exists=True))
+@click.argument('function_dir', type=click.Path(exists=True))
+def run(sequence_file, function_dir):
+    """Run a sequence.
+
+    SEQUENCE_FILE is the path to the sequence file to check.
+    FUNCTION_DIR is the path to the directory containing Python files
+    and functions referenced by the sequence.
+    """
+    runner = SequenceRunner(function_dir, sequence_file)
+    runner.run(blocking=True)
